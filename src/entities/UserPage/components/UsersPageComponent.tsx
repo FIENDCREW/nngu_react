6af112f0ead079b1a338a5d0acd1d68a1c2d2b18
FIENDCREW <odysseybmx911@gmail.com>
2022-05-21 12:12:41 +0300

    new

--- src/entities/UserPage/components/UsersPageComponent.tsx
+++ src/entities/UserPage/components/UsersPageComponent.tsx
@@ -1,11 +1,34 @@
-import React from "react";
-
-interface IUsers {
-  usersData: IUsers | null;
+import React, { FC } from 'react';
+import { IUsers } from '../../../layouts/PageLayout/interface';
+import style from '../UsersPage.module.scss';
+import UserCard from './UserCard';
+interface IProps {
+  usersDataAttr: IUsers[] | null;
 }
 
-const UsersPageComponent: FC<IUsers> = () = > {
-  return <div>Component</div>;
-}; 
+const UsersPageComponent: FC<IProps> = ({ usersDataAttr }) => {
+  if (!usersDataAttr) {
+    return <div>Загрузка...</div>;
+  }
+  return (
+    <div>
+      <p>Список пользователей:</p>
+      <div className={style.user_list}>
+        {usersDataAttr.length ? (
+          usersDataAttr.map((user, i) => {
+            const { id, name, email } = user;
+            return (
+              <React.Fragment key={`UserId:${id}`}>
+                <UserCard name={name} email={email} />
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <div>Пользователи не найдены.</div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default UsersPageComponent;
--- src/entities/UserPage/index.tsx
+++ src/entities/UserPage/index.tsx
@@ -5,15 +5,20 @@ import { IUsers } from '../../layouts/PageLayout/interface';
 import UsersPageComponent from './components/UsersPageComponent';
 
 const UserPage = () => {
-  const [usersData, setUserdData] = useState<IUsers | null>(null);
-  useEffect(() => {
-    fetch(userUrl)
-      .then((responce) => responce.json())
-      .then((data) => setUserdData(data));
-  }, []);
-  console.log(usersData);
+  const [usersData, setUsersdData] = useState<IUsers[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  return <UsersPageComponent />;
+  useEffect(() => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      fetch(userUrl)
+        .then((responce) => responce.json())
+        .then((data) => setUsersdData(data));
+    }, 1500);
+  }, []);
+
+  return !usersData ? <div>Загрузка...</div> : <UsersPageComponent usersDataAttr={usersData} />;
 };
 
 export default UserPage;
