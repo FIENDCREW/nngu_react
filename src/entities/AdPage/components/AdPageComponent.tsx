0338e4705ef669ba46cc65d9abaa69e5142b4a65
FIENDCREW <odysseybmx911@gmail.com>
2022-07-06 17:57:19 +0300

    + backup

--- src/entities/AdPage/components/AdPageComponent.tsx
+++ src/entities/AdPage/components/AdPageComponent.tsx
@@ -1,44 +1,49 @@
 import React, { useState } from 'react';
 import style from './AdPage.module.scss';
+import BackupComponent from './BackupComponent';
 
 const AdPageComponent = () => {
   const [value, setValue] = useState('text');
   function handleChange(event: { target: { value: React.SetStateAction<string> } }) {
     setValue(event.target.value);
   }
+  const [textarea, setTextarea] = useState('');
   return (
-    <div className={style.main_container}>
-      <div className={style.main_container_name}>
-        <p>Название продукта</p>
-        <input type="text" name="name product" id="" value={value} onChange={handleChange} />
-      </div>
-      <div className={style.main_container_category}>
-        <div>
-          <p>Категория</p>
-          <input type="text" name="category" id="" />
+    <>
+      <BackupComponent />
+      <div className={style.main_container}>
+        <div className={style.main_container_name}>
+          <p>Название продукта</p>
+          <input type="text" name="name product" id="" value={value} onChange={handleChange} />
         </div>
-        <div className={style.main_container_much}>
-          <p>Стоимость</p>
-          <input type="text" name="so much" id="" />
+        <div className={style.main_container_category}>
+          <div>
+            <p>Категория</p>
+            <input type="text" name="category" id="" />
+          </div>
+          <div className={style.main_container_much}>
+            <p>Стоимость</p>
+            <input type="text" name="so much" id="" />
+          </div>
+        </div>
+        <div className={style.main_container_phone}>
+          <p>Телефон</p>
+          <input type="tel" name="Phone" id="" />
+        </div>
+        <div className={style.main_container_description}>
+          <p>Описание</p>
+          <textarea value={textarea} onChange={event => setTextarea(event.target.value)} />
+        </div>
+        <div className={style.main_container_photo}>
+          <p>Фотография</p>
+          <input type="file" name="file" id="" />
+        </div>
+        <div className={style.main_container_location}>
+          <p>Место положение</p>
+          <input type="location" />
         </div>
       </div>
-      <div className={style.main_container_phone}>
-        <p>Телефон</p>
-        <input type="tel" name="Phone" id="" />
-      </div>
-      <div className={style.main_container_description}>
-        <p>Описание</p>
-        <input type="text" name="Description" id="" />
-      </div>
-      <div className={style.main_container_photo}>
-        <p>Фотография</p>
-        <input type="image" src="Photo" alt="no photo" />
-      </div>
-      <div className={style.main_container_location}>
-        <p>Место положение</p>
-        <input type="location" />
-      </div>
-    </div>
+    </>
   );
 };
 
