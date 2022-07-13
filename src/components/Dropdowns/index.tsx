f43812ebe5a855802c8f9b6fb7d2ee1c625dc570
FIENDCREW <odysseybmx911@gmail.com>
2022-07-13 14:05:45 +0300

    scss

--- src/components/Dropdowns/index.tsx
+++ src/components/Dropdowns/index.tsx
@@ -4,30 +4,30 @@ import { BtnList } from './btnList';
 import style from './Dropdowns.module.scss';
 
 const Dropdowns = () => {
-  const [ isVisible, setISVisible] = useState(false);
+  const [isVisible, setISVisible] = useState(false);
   const { pathname } = useLocation();
-    return (
-      <div className={style.menu_container}>
-        <div onClick={() => setISVisible(prevState => !prevState)}>|||</div>
-          {isVisible && 
-            <div>
-                <ul className={style.dropdown_menu}>
-                  {BtnList.map((menuItem) => {
-                    const isActiveClassName = pathname.includes(menuItem.path) ? style.active : '';
-                      return (
-                        <Link
-                          key={menuItem.id}
-                          to={menuItem.path}
-                          className={`${isActiveClassName} ${style.dropdown_menu_item}`}>
-                          <li className={style.dropdown_menu_name}>{menuItem.text}</li>
-                        </Link>
-                      );
-                  })}
-                </ul>
-            </div>
-          }
-      </div>
-    );
+  return (
+    <div className={style.menu_container}>
+      <div onClick={() => setISVisible((prevState) => !prevState)}>|||</div>
+      {isVisible && (
+        <div>
+          <ul className={style.dropdown_menu}>
+            {BtnList.map((menuItem) => {
+              const isActiveClassName = pathname.includes(menuItem.path) ? style.active : '';
+              return (
+                <Link
+                  key={menuItem.id}
+                  to={menuItem.path}
+                  className={`${isActiveClassName} ${style.dropdown_menu_item}`}>
+                  <li className={style.dropdown_menu_name}>{menuItem.text}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Dropdowns;
