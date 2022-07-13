e1d1f6029e35476ec3a69cc96468b15c33f34c2d
FIENDCREW <odysseybmx911@gmail.com>
2022-07-13 11:48:11 +0300

    dropdown

--- src/components/Dropdowns/index.tsx
+++ src/components/Dropdowns/index.tsx
@@ -1,12 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Value } from 'sass';
+import { BtnList } from './btnList';
 import style from './Dropdowns.module.scss';
 
 const Dropdowns = () => {
+  const [ isVisible, setISVisible] = useState(false);
+  const { pathname } = useLocation();
   return (
     <div className={style.menu_container}>
-      <div>
-        <div></div>
-      </div>
+      <div onClick={() => setISVisible(prevState => !prevState)}>|||</div>
+      {isVisible && 
+        <div>
+            <ul className={style.sidebar_menu}>
+              {BtnList.map((menuItem) => {
+                const isActiveClassName = pathname.includes(menuItem.text) ? style.active : '';
+                  return (
+                    <Link
+                      key={menuItem.id}
+                      to={menuItem.text}
+                      className={`${isActiveClassName} ${style.sidebar_menu_item}`}>
+                      <li className={style.sidebar_menu_name}>{menuItem.text}</li>
+                    </Link>
+                  );
+              })}
+            </ul>
+        </div>
+      }
     </div>
   );
 };
